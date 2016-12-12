d559a1df49e3ff12f527f12a7070c3222e3641bd
Josh Ladisic <jal5313@gmail.com>
2016-12-12 14:27:31 -0500

    Update Model to reflect all fields

--- src/NYCTaxiViewer/Data/DbInitializer.cs
+++ src/NYCTaxiViewer/Data/DbInitializer.cs
@@ -21,9 +21,28 @@ namespace NYCTaxiViewer.Models
             //add test data if the database is empty.
             var taxiTrips = new TaxiTrip[]
             {
-                new TaxiTrip{pickUpLat=12, pickUpLon=123, dropOffLat=1.2f, pdropOffLat=12},
-                new TaxiTrip{pickUpLat=17, pickUpLon=124, dropOffLat=1.6f, pdropOffLat=17},
-                new TaxiTrip{pickUpLat=124, pickUpLon=17, dropOffLat=12, pdropOffLat=1.2f}
+                new TaxiTrip
+                {
+                    VendorId = 2,
+                    tpep_pickup_datetime = "1/1/2016 0:00:00",
+                    tpep_dropoff_datetime = "1/1/2016 0:00:00",
+                    passenger_count = 2,
+                    trip_distance = 1.1f,
+                    pickup_longitude = -73.9903717,
+                    pickup_latitude = 40.73469543,
+                    RatecodeID = 1,
+                    store_and_fwd_flag = 'N',
+                    dropoff_longitude = -73.98184204,
+                    dropoff_latitude = 40.73240662,
+                    payment_type = 2,
+                    fare_amount = 7.5f,
+                    extra = 0.5f,
+                    mta_tax = 0.5f,
+                    tip_amount = 0,
+                    tolls_amount = 0,
+                    improvement_surcharge = .03f,
+                    total_amount = 8.8f
+                }
             };
             foreach (TaxiTrip s in taxiTrips)
             {
--- src/NYCTaxiViewer/Models/TaxiTrip.cs
+++ src/NYCTaxiViewer/Models/TaxiTrip.cs
@@ -8,9 +8,24 @@ namespace NYCTaxiViewer.Models
     public class TaxiTrip
     {
         public int ID { get; set; }
-        public float pickUpLat { get; set; }
-        public float pickUpLon { get; set; }
-        public float dropOffLat { get; set; }
-        public float pdropOffLat { get; set; }
+        public int VendorId { get; set; }
+        public string tpep_pickup_datetime { get; set; }
+        public string tpep_dropoff_datetime { get; set; }
+        public int passenger_count { get; set; }
+        public float trip_distance { get; set; }
+        public double pickup_longitude { get; set; }
+        public double pickup_latitude { get; set; }
+        public int RatecodeID { get; set; }
+        public char store_and_fwd_flag { get; set; }
+        public double dropoff_longitude { get; set; }
+        public double dropoff_latitude { get; set; }
+        public int payment_type { get; set; }
+        public float fare_amount { get; set; }
+        public float extra { get; set; }
+        public float mta_tax { get; set; }
+        public float tip_amount { get; set; }
+        public float tolls_amount { get; set; }
+        public float improvement_surcharge { get; set; }
+        public float total_amount { get; set; }
     }
 }
